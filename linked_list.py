02bbc588a72530ac0fc284bed75d803d7e01550e
Wilmo95 <bartosz.wilmowicz@gmail.com>
2022-10-17 18:57:57 +0200

    Add linked list

---
+++ linked_list.py
@@ -0,0 +1,176 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return temp
+        else:
+            temp = self.head
+            self.head = temp.next
+            temp.next = None
+            self.length -= 1
+            return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        else:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            return temp
+
+    def set(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        else:
+            return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+
+        if index == 0:
+            return self.prepend(value)
+
+        if index == self.length:
+            return self.append(value)
+
+        temp = self.get(index - 1)
+        new_node = Node(value)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            self.pop_first()
+        if index == self.length - 1:
+            self.pop()
+
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def revese(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+if __name__ == "__main__":
+    linked_list = LinkedList(5)
+    linked_list.print_list()
+    print()
+
+    linked_list.append(6)
+    linked_list.append(7)
+    linked_list.print_list()
+    print()
+
+    print(linked_list.pop().value)
+    print()
+    linked_list.print_list()
+    print()
+
+    linked_list.prepend(1)
+    linked_list.print_list()
+
+    print()
+
+    linked_list.pop_first()
+    linked_list.print_list()
+
+    print()
+    linked_list.append(10)
+    linked_list.append(9)
+    linked_list.append(4)
+    linked_list.print_list()
+    print()
+    print(linked_list.get(3).value)
+    print()
+    linked_list.set(3, 99)
+    linked_list.print_list()
+    print()
+    linked_list.remove(2)
+    linked_list.print_list()
+    print()
+
+    linked_list.revese()
+    linked_list.print_list()
